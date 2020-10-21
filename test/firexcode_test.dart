c0ffc594bf0a0d999c9e0e0e19eae83b1bec717c
zeeshux7860 <zeeshux7860.zx@gmail.com>
2020-10-21 21:24:13 +0530

    first commit

---
+++ test/firexcode_test.dart
@@ -0,0 +1,26 @@
+import 'package:firexcode/firexcode.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return HomePage().xMaterialApp();
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return xListView.list([
+
+    ]).xScaffold();
+  }
+}
